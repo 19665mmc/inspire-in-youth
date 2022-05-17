95191fcc2d70c96a7962ee68f177456dd41bd07c
martinchege <chegembugua2003@gmail.com>
2022-05-17 20:41:41 +0300

    lesson 2 about integers

---
+++ assignment.py
@@ -0,0 +1,46 @@
+#area of circle
+from operator import length_hint
+
+
+input("areaofcircle")
+input("radius=7")
+input("pi=22/7")
+radius=7
+pi=22/7
+area=pi*radius*radius
+print("area"+str(area))
+
+#volume of cuboid
+input("volumeofcuboid")
+input("length=7")
+input("width=6")
+input("height=5")
+length=7
+width=6
+height=5
+volume=length*width*height
+print("volume"+str(volume))
+
+#volumeofcylinder
+input("volumeofcylinder")
+input("radius=14")
+input("pi=22/7")
+input("height=10")
+radius=14
+pi=22/7
+volume=pi*radius*radius*height
+print("volume"+str(volume))
+
+
+#surfaceareaofcylinder
+input("surfaceareaofcylinder")
+input("radius=7")
+input("pi=22/7")
+input("height=14")
+radius=7
+pi=22/7
+height=14
+surfacearea=(pi*radius*radius)+(pi*(radius+radius)*height)
+print("surfacearea"+str(surfacearea))
+
+print("presented by martin mbugua (^-^)")
