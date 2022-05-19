a3a90563dcec10a4dc63a187881a108e47ed7a01
martinchege <chegembugua2003@gmail.com>
2022-05-19 11:15:21 +0500

    loop 0

---
+++ lesson7.py
@@ -0,0 +1,18 @@
+#learning about lists
+motorcycle=['honda','suzuki','yamaha']
+motorcycle_owner="Deku"
+#accessing indices
+#print(motorcycle[1])
+plate_number =['767676','200034','45466']
+#print(motorcycle[0],plate_number[0])
+#print(motorcycle[1],plate_number[1])
+#deleting item from list
+#del motorcycle[0]
+#print(motorcycle)
+#popped_motorcycle = motorcycle.pop()
+#print(motorcycle)
+#print statement
+#my name is Deku and i own a motorcycle plate no.
+#print("my name is "+str(motorcycle_owner)+" and i own a "+str(motorcycle[0])+" plate number "+str(plate_number[0]))
+#motorcycle.remove('honda')
+#print(motorcycle)
---
+++ loop.py
@@ -0,0 +1,12 @@
+#kfk
+school=['hope','joy','gifted','happy']
+pupil=['martin','mbugua','chege','soshi']
+#print(pupil[0], school[0])
+#print(pupil[1], school[1])
+#print(pupil[2],school[2] )
+#print(pupil[3],school[3])
+for pupil in pupil:
+ print(f'Hello I am pupil {pupil}')
+
+
+
